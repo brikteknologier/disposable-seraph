741ed95b313ec62e8847c8404b97cd2970b41e2f
Jon Packer <contact@jonpacker.com>
2013-03-08 23:26:44 +0100

    save port between runs

--- disposable-seraph.js
+++ disposable-seraph.js
@@ -3,16 +3,27 @@ var nsv = require('neo4j-supervisor'),
     seraph = require('seraph'),
     rand = require('randy').randInt,
     async = require('async'),
-    naan = require('naan');
+    naan = require('naan'),
+    fs = require('fs');
 
 module.exports = function(opts, cb) {
   if (typeof opts == 'function') cb = opts;
   
   var version = opts.version || '1.9.M05';
   var edition = opts.edition || 'community';
-  var port = opts.port || rand(20000, 60000);
+  var port;
+
+  var getPort = function(cb) {
+    fs.readFile(__dirname + '/neo4j.port', 'utf8', function(err, portstr) {
+      port = !err && portstr ? parseInt(portstr, 10) : rand(20000, 60000);
+      fs.writeFile(__dirname + '/neo4j.port', 'utf8', function(err) {
+        cb(err);
+      });
+    });
+  };
 
   async.waterfall([
+    getPort,
     function getNeoInstall(cb) { nvm(version, edition, cb) },
     function createSupervisor(loc, cb) { cb(null, nsv(loc)) },
     function setPort(neo, cb) { naan.b.wrap(neo, neo.port, neo)(port, cb) },
